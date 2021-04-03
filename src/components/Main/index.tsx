88e29eae7593f11256de4c6f7dc052e94feba941
Alden Fachetti <aldenmerlin@gmail.com>
2021-04-02 21:31:42 -0300

    first component and improving snapshots

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,21 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo simbolizando o icone do ReactJS com um texto ao lado escrito React Avançado"
+    />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      A simple project starter to work with TypeScript, React, NextJS and
+      Styled-Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Imagem ilustrativa do mascote do GitHub vestido de um astronauta "
+    />
+  </S.Wrapper>
 )
 
 export default Main
