79ac3eec8897fca5ead181349e618ed4452b55a7
Alden Fachetti <aldenmerlin@gmail.com>
2021-04-11 10:54:47 -0300

    update component home - styles and test

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,18 +1,18 @@
 import * as S from './styles'
 
 const Main = ({
-  title = 'React Avançado',
+  title = 'React Boilerplate',
   description = 'A simple project starter to work with TypeScript, React, NextJS and Styled-Components'
 }) => (
   <S.Wrapper>
     <S.Logo
-      src="/img/logo.svg"
+      src="/img/logo.png"
       alt="Imagem de um átomo simbolizando o icone do ReactJS com um texto ao lado escrito React Avançado"
     />
     <S.Title>{title}</S.Title>
     <S.Description>{description}</S.Description>
     <S.Illustration
-      src="/img/hero-illustration.svg"
+      src="/img/hero-illustration.png"
       alt="Imagem ilustrativa do mascote do GitHub vestido de um astronauta "
     />
   </S.Wrapper>
